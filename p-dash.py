adf5baa9cdd2dc3c31185ed6066bce3da8dfca45
Chase Roohms <131704514+ChaseRoohms@users.noreply.github.com>
2024-07-26 16:06:30 -0500

    Add files via upload

--- p-dash.py
+++ p-dash.py
@@ -1,27 +1,11 @@
-import sys
+import os
 import socket
 import threading
 import time
-import re
+import argparse
 from queue import Queue
 from _socket import getservbyport
 
-global scan_all
-global max_port
-
-ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-
-# ====================================================== Strings ======================================================
-version = 'v.1.0'
-
-logo = (
-    "██████╗       ██████╗  █████╗ ███████╗██╗  ██╗\n"
-    "██╔══██╗      ██╔══██╗██╔══██╗██╔════╝██║  ██║\n"
-    "██████╔╝█████╗██║  ██║███████║███████╗███████║\n"
-    "██╔═══╝ ╚════╝██║  ██║██╔══██║╚════██║██╔══██║\n"
-    "██║           ██████╔╝██║  ██║███████║██║  ██║\n"
-    "╚═╝           ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n"
-)
 
 # =================================================== Formatting Stuff ================================================
 WHITE = '\033[97m'
@@ -36,147 +20,267 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
 
+# ====================================================== Strings ======================================================
+ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+version = 'v.1.0'
+
+logo = (
+    " ██████╗       ██████╗  █████╗ ███████╗██╗  ██╗\n"
+    " ██╔══██╗      ██╔══██╗██╔══██╗██╔════╝██║  ██║\n"
+    " ██████╔╝█████╗██║  ██║███████║███████╗███████║\n"
+    " ██╔═══╝ ╚════╝██║  ██║██╔══██║╚════██║██╔══██║\n"
+    " ██║           ██████╔╝██║  ██║███████║██║  ██║\n"
+    " ╚═╝           ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n"
+)
+
+
+help_string = (
+        "<Description>\n" +
+        BOLD + "Usage: {} <target IP> [--all]" + END + "\n" +
+        "\n" +
+        "Commands:\n" +
+        "         <target IP>            Scans this IP address\n" +
+        "         --version              Print version information and exit\n" +
+        "Options:\n" +
+        "     -a, --all                  Scans all ports from 1-65535\n"
+)
+
+
+# ===================================================== Functions =====================================================
 def logo_format():
+    """
+    Color shadow symbols ╠ ╣ ╔ ╗ ╚ ╝ ║  light gray, and the letter symbols █ white
+    :return:
+    """
     formatted_logo = WHITE
     gray = False
-    shadowed_symbols = ['╗', '╔', '═', '╝', '╚', '║']
-    for symbol in logo:
-        if symbol in shadowed_symbols:
+    shadowed_symbols = ['╔', '╗', '═', '╚', '╝', '║', '╠', '╣']
+    for symbol in logo:  # Process every character
+        if symbol in shadowed_symbols:  # Light gray before shadowed symbols
             if not gray:
                 formatted_logo += LG
                 gray = True
-            formatted_logo += symbol
-        else:
+        else:                           # White before letter symbols
             if gray:
                 formatted_logo += WHITE
                 gray = False
-            formatted_logo += symbol
+        formatted_logo += symbol        # Add the symbol
 
-    formatted_logo += WHITE + BOLD + RED + ('%46s' % ("                                  - " + version)) + END
+    # Add the version below the logo
+    formatted_logo += WHITE + BOLD + RED + ('%46s' % ("- " + version)) + END
     return formatted_logo
 
 
-# Prints script usage
-def usage():
-    print("Usage: " + sys.argv[0] + " <target IP> [--all]")
-    print("     --help for more information")
-
-
-# Prints the --help screen information
-def help_screen():
+def print_results(ip_address, open_ports_dict, runtime):
+    """
+    Pretty print the results of a port scan
+    :param ip_address: The IP address scanned to get these results
+    :param open_ports_dict: A dictionary of open ports 'port':'service'
+    :param runtime: How long the program ran for
+    :return:
+    """
+    print('╔' + '═' * 45 + '╗')
+    print('║ ' + ('%-29s%-23s' % ((BOLD + "Target" + END), ip_address)) + '║')
+    print('╠' + '═' * 45 + '╣')
+    if len(open_ports_dict) == 0:
+        print('║ ' + ('%-44s' % 'No open ports found') + '║')
+    else:
+        print('║ ' + ('%-25s%-23s' % ((BOLD + "Port"), "Service")) + END + '║')
+        for port, service in open_ports_dict.items():
+            print('║ ' + ('%-21s%-23s' % (port, service)) + '║')
+    print('╠' + '═' * 45 + '╣')
+    print('║ ' + ('%44s' % (str(round(runtime, 2)) + ' Secs ') + '║'))
+    print('╚' + '═' * 45 + '╝')
     print()
-    print(logo_format())
-    print("Description.")
-    print(BOLD + "Usage: " + sys.argv[0] + " <target IP> [--all]" + END)
-    print()
-    print("Commands:")
-    print("         <target IP>            Scans this IP address")
-    print("         --version              Print version information and exit")
-    print("Options:")
-    print("     -a, --all                  Scans all ports from 1-65535")
 
 
-# Prints the --version screen information
-def version_screen():
-    print()
-    print(logo_format())
+def print_error(ip_address, error_message):
+    """
+    Pretty print the error message
+    :param ip_address: The IP address scanned that threw an error
+    :param error_message: The error message you wish a user to see
+    :return:
+    """
+    print('╔' + '═' * 45 + '╗')
+    print('║ ' + ('%-29s%-23s' % ((BOLD + "Target" + END), ip_address)) + '║')
+    print('╠' + '═' * 45 + '╣')
+    print('║ ' + ('%-53s' % (BOLD + RED + error_message)) + END + '║')
+    print('╚' + '═' * 45 + '╝')
 
 
-# Ensures correct args and handles them
-def arg_handler(argv):
-    global max_port
-    max_port = 10000
-    arg_count = len(argv)
-
-    match arg_count:
-
-        case 2:                                     # <Program Name> <first_argument>
-            match argv[1]:
-                case '--version':                       # <Program Name> --version
-                    version_screen()                        # Print Version information
-                    quit()
-                case '--help' | '-h' | 'help':          # <Program Name> <--help or -h or help>
-                    help_screen()                           # Print help information
-                    quit()
-
-        case 3:                                     # <Program Name> <first_argument> <second_argument>
-            match argv[2]:
-                case '--all' | '-a':                # <Program Name> <first_argument> <--all or -a>
-                    max_port = 65535
-                case _:                                 # <Program Name> <first_argument> <invalid argument>
-                    usage()                                 # Invalid second argument
-                    quit()
-
-        case _:                                     # <Program Name> <wrong amount of arguments>
-            print()
-            usage()                                     # Invalid number of arguments
-            quit()
-
-    if not re.match(ip_pattern, argv[1]):
-        print()
-        print(RED + "Invalid IP address" + END)
-        quit()
+def exiting():
+    """
+    Exits the program gracefully
+    :return:
+    """
+    print("\r", end="")
+    print('Exiting...')
+    quit()
 
 
-socket.setdefaulttimeout(0.25)
-print_lock = threading.Lock()
-target = re.search(ip_pattern, sys.argv[1]).group(0)
+def ping_ip(ip_address):
+    """
+    Tries to ping an ip address to determine if its accessible
+    :param ip_address: The IP address to scan
+    :return:
+    """
+    try:
+        response = os.popen(f"ping {ip_address} ").read()                       # Ping the address and save the response
+        if "Request timed out." in response or "unreachable" in response:       # Not pingable
+            print_error(ip_address, "Unable to ping target...")
+            return False
+        elif "could not find" in response:                                      # Doesn't exist
+            print_error(ip_address, "Could not find target...")
+            return False
+        else:                                                                   # Successful ping
+            return True
+    except KeyboardInterrupt:
+        exiting()
 
 
 # noinspection PyBroadException
-def portscan(port_to_scan):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def portscan(ip_address, port_to_scan, open_ports):
+    """
+    Scans an IP address to see if a port is open, adds it to open_ports if it is
+    :param ip_address: The IP address to scan
+    :param port_to_scan: The port to check
+    :param open_ports: Dictionary of open ports and the services they run 'port':'service'
+    :return:
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # New socket
     try:
-        s.connect((target, port_to_scan))
-        with print_lock:
-            try:
-                service = getservbyport(port_to_scan)
-            except:
-                service = "n/a"
-            print('║ ' + ('%-20s%-23s' % (port_to_scan, service)) + '║')
+        s.connect((ip_address, port_to_scan))                   # Try to connect to IP and port
+        try:
+            service = getservbyport(port_to_scan)               # Look for known port - service relationship
+        except OSError:
+            service = "n/a"                                     # No known service
+        open_ports[port_to_scan] = service
         s.close()
     except:
         pass
 
 
-def threader():
+def threader(ip_address, open_ports):
+    """
+    The threaded function for parallel processing
+    :param ip_address: The IP address to scan
+    :param open_ports: Dictionary of open ports and the services they run 'port':'service'
+    :return:
+    """
     while True:
-        worker = q.get()
-        portscan(worker)
-        q.task_done()
+        port = q.get()                              # Get next port
+        portscan(ip_address, port, open_ports)      # Scan that port
+        q.task_done()                               # Remove from queue
 
 
-arg_handler(sys.argv)
+def scan_ip(ip_address, max_port, thread_count, open_ports):
+    """
+    Scans an IP address for all open ports between 1 and max_port. Executes on thread_count parallel threads.
+    :param ip_address: The IP address to scan
+    :param max_port: Maximum number of ports to scan. Scans 1-max_port
+    :param thread_count: Maximum number of threads to run in parallel
+    :param open_ports: Dictionary of open ports and the services they run 'port':'service'
+    :return:
+    """
+    for x in range(thread_count):  # Create threader with thread_count threads
+        t = threading.Thread(target=threader, args=(ip_address, open_ports))    # Feed the threaders >:)
+        t.daemon = True
+        t.start()
+
+    for port in range(1, max_port):     # Scan ports 1-max_port
+        q.put(port)
+
+    q.join()    # BEGIN
+
+
+def get_arg_parser():
+    """
+    Creates an argeparse parser to gather CLI arguments
+    :return:
+    """
+    parser = argparse.ArgumentParser(
+        prog='p-dash')
+    parser.add_argument(
+        'ip_address',
+        help='IP address to scan')
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s ' + version)
+    parser.add_argument(
+        '-a', '--all',
+        action='store_const',
+        const=True,
+        default=False,
+        help='scan all ports from 1-65535, default 1-10000')
+    parser.add_argument(
+        'speed',
+        default='--fast',
+        const='--fast',
+        nargs='?',
+        choices=['--fast', '--med', '--slow'],
+        help='the speed at which you want to scan (default: %(default)s)')
+    return parser
+
+
+def get_args():
+    """
+    Gets arguments ip_address, max_port and thread_count and returns them
+    :return:
+    """
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    ip_address = args.ip_address
+
+    if args.all:
+        max_port = 65535
+    else:
+        max_port = 10000
+
+    match args.speed:
+        case '--fast':
+            thread_count = 1000
+        case '--medium':
+            thread_count = 100
+        case '--slow':
+            thread_count = 10
+        case _:
+            thread_count = 1
+
+    return ip_address, max_port, thread_count
+
+
+def run(ip_address, max_port, thread_count):
+    """
+    Scans an IP address for all open ports, and then pretty prints the results
+    :param ip_address:
+    :param max_port:
+    :param thread_count:
+    :return:
+    """
+    start_time = time.time()
+
+    socket.setdefaulttimeout(0.25)
+
+    open_ports = dict()
+    try:
+        if ping_ip(ip_address):
+            scan_ip(ip_address, max_port, thread_count, open_ports)
+            print_results(ip_address, open_ports, time.time() - start_time)
+    except KeyboardInterrupt:
+        exiting()
+
+
+# ==================================================== Main Program ===================================================
+print_lock = threading.Lock()
 q = Queue()
-startTime = time.time()
+
+target, port_limit, thread_limit = get_args()
 
 print()
 print(logo_format())
-print()
-print('╔' + '═' * 44 + '╗')
-print('║ ' + ('%-28s%-23s' % ((BOLD + "Target" + END), target)) + '║')
-print('╠' + '═' * 44 + '╣')
-print('║ ' + ('%-24s%-23s' % ((BOLD + "Open Port"), "Service")) + END + '║')
+print("Scanning...")
 
-for x in range(1000):
-    t = threading.Thread(target=threader)
-    t.daemon = True
-    t.start()
-
-for port in range(1, max_port):
-    q.put(port)
-
-try:
-    q.join()
-except KeyboardInterrupt:
-    print('║ ' + ('%-43s' % 'Exiting...') + '║')
-    print('╚' + '═' * 44 + '╝')
-    quit()
-except socket.error:
-    print('║ ' + ('%-43s' % 'Server not responding...') + '║')
-
-print('╠' + '═' * 44 + '╣')
-print('║ ' + ('%-43s' % ('Time taken: ' + str(round((time.time() - startTime), 2)) + ' seconds') + '║'))
-print('╚' + '═' * 44 + '╝')
-print()
-
+run(target, port_limit, thread_limit)
